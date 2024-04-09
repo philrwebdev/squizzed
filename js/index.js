79e625260d93bfc28e29bfaaf5877bd767793e9b
winget <philip.rausch@gmail.com>
2024-04-09 18:30:20 +0200

    Implement letter-wise un/blurring of answer text

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ const bodyElement = document.querySelector("[data-js=body]");
 const mainElement = document.querySelector("[data-js=main]");
 const cardsElement = document.querySelector("[data-js=cards]");
 
+function lettersToSpans(element) {
+  const split = element.innerHTML.split("");
+  console.log(element);
+  console.log(split);
+
+  let letterSpans = "";
+  for (i = 0; i < split.length; i++) {
+    letterSpans +=
+      '<span class="letterSpan" position=' + i + ">" + split[i] + "</span>";
+  }
+
+  console.log(split);
+
+  return letterSpans;
+}
+
 window.onload = function () {
   // Check if localStorage is available (IE8+) and make sure that the visited flag is not already set.
   if (
@@ -20,6 +36,19 @@ document.addEventListener("DOMContentLoaded", () => {
     cardsElement.innerHTML += cardsIndexIn;
   }
 });
+
+window.onload = (event) => {
+  for (let i = 0; i < cardsElement.childElementCount; i++) {
+    let currentSolution = cardsElement.children[i].querySelector(
+      "[data-js = solution]"
+    );
+    if (!currentSolution.classList.contains("spanified")) {
+      let spanifiedSolution = lettersToSpans(currentSolution);
+      currentSolution.innerHTML = spanifiedSolution;
+      currentSolution.classList.toggle("spanified");
+    }
+  }
+};
 
 window.onbeforeunload = () => {
   const cardsIndexOut = cardsElement.querySelectorAll("[data-js=card]");
@@ -42,6 +71,18 @@ mainElement.addEventListener("click", (event) => {
     );
     targetAnswer.classList.toggle("visible");
 
+    const nLetters = targetAnswer.childElementCount;
+    let targetAnswerLetters = targetAnswer.querySelectorAll(".letterSpan");
+
+    for (i = 0; i < nLetters; i++) {
+      targetAnswerLetters[i].classList.toggle("visible");
+      if (targetAnswerLetters[i].classList.contains("visible")) {
+      targetAnswerLetters[i].style.transitionDelay = 50 * i + "ms";
+    } else {
+      targetAnswerLetters[i].style.transitionDelay = -50 * i + "ms";
+    }
+    }
+
     if (targetAnswer.classList.contains("visible")) {
       event.target.textContent = "Hide Answer";
     } else {
@@ -53,3 +94,6 @@ mainElement.addEventListener("click", (event) => {
     targetCard.classList.toggle("bookmarked");
   }
 });
+
+
+
